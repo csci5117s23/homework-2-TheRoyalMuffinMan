1766a077f5310ffceb6af2c31eff18ac2f91931a
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2023-04-02 23:43:13 -0500

    initial setup

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import { chakra } from '@chakra-ui/react';
+
+export default function Home() {
+    return (
+        <chakra.div 
+            display="flex" 
+            flexDirection="column" 
+            minHeight="100vh"
+            bg="#eee"
+        >
+            hello world
+        </chakra.div>
+    ); 
+}
